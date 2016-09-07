e9446b726bf1df491219a034e6ac5c17b84e1212
engle2192 <go2engle@gmail.com>
2016-09-06 23:02:42 -0400

    Updated files needed

---
+++ Files for pokemongo-map directory/RunNotifications.bat
@@ -0,0 +1,3 @@
+@echo off
+python runwebhook.py -H YOUR_IP -P YOUR_WEBHOOK_PORT
+pause
--- Files for pokemongo-map directory/install.bat
+++
@@ -1,3 +0,0 @@
-pip install git+https://github.com/sriyegna/pikaptcha
-pip install git+https://github.com/keyphact/pgoapi.git
-pip install -r requirements.txt
