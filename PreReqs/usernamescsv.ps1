e8ed46467aafd71eba32127f2432a8cc2413f6aa
ferrotr1 <ferrotr@yahoo.com>
2016-09-19 19:10:34 -0400

    Fix error that happens when all user accounts are banned

--- PreReqs/usernamescsv.ps1
+++ PreReqs/usernamescsv.ps1
@@ -8,11 +8,19 @@ foreach($username in $banned)
     $usernames = $usernames | where {$_ -notmatch $username}
     }
 $usernames = $usernames | where {$_ -notlike ""}
-[System.IO.File]::WriteAllLines($usernamesPath, $usernames)
 remove-item -Path $bannedpath -Force -ErrorAction SilentlyContinue
-foreach($username in $usernames)
-    {
-    $usernames = $usernames.replace($username,("ptc," + $username))
-    }
-$usernames = $usernames.replace(":",",")
-[System.IO.File]::WriteAllLines($csvpath, $usernames)
+If($usernames)
+{
+    [System.IO.File]::WriteAllLines($usernamesPath, $usernames)
+    foreach($username in $usernames)
+        {
+        $usernames = $usernames.replace($username,("ptc," + $username))
+        }
+    $usernames = $usernames.replace(":",",")
+    [System.IO.File]::WriteAllLines($csvpath, $usernames)
+}
+Else
+{
+    Remove-Item -Path $usernamespath -Force
+    Remove-Item -Path $csvpath -Force -ErrorAction SilentlyContinue
+}
